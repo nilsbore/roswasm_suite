172412fe551e53433d23737c3873300da9f85184
Nils Bore <nbore@kth.se>
2020-04-04 21:44:20 +0200

    Got somewhere with timers

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@
 #include <rosmon_msgs/StartStop.h>
 #include <iostream>
 
+namespace roswasm_webgui {
+
 struct LaunchState {
 
     static const char* status[];
@@ -25,13 +27,14 @@ struct LaunchState {
     roswasm::Subscriber* sub;
     roswasm::ServiceClient* client;
     rosmon_msgs::State msg;
-    std::string topic;
+    std::string name;
     int selected;
     //size_t nbr_nodes;
     
     int start_stop_launch_node_idx;
     int start_stop_launch_attempts;
     uint8_t start_stop_launch_action;
+    int max_launch_attempts;
 
     void service_callback(const rosmon_msgs::StartStop::Response& res, bool result)
     {
@@ -67,6 +70,9 @@ struct LaunchState {
             if (state == rosmon_msgs::NodeState::CRASHED) {
                 state = rosmon_msgs::NodeState::IDLE;
             }
+        }
+        if (start_stop_launch_attempts > max_launch_attempts) {
+            start_stop_launch_node_idx = -1;
         }
         if (start_stop_launch_node_idx != -1) {
             start_stop(start_stop_launch_action, msg.nodes[start_stop_launch_node_idx].name, msg.nodes[start_stop_launch_node_idx].ns);
@@ -113,12 +119,13 @@ struct LaunchState {
         }
     }
 
-    LaunchState(roswasm::NodeHandle* node_handle, const std::string& topic) : topic(topic), selected(-1), start_stop_launch_node_idx(-1)
+    LaunchState(roswasm::NodeHandle* node_handle, const std::string& topic, int max_launch_attempts = 10) : selected(-1), start_stop_launch_node_idx(-1), max_launch_attempts(max_launch_attempts)
     {
         sub = node_handle->subscribe<rosmon_msgs::State>(topic, std::bind(&LaunchState::callback, this, std::placeholders::_1));
         //topics_service = nh->serviceClient<rosapi::TopicsForType>("/rosapi/topics_for_type", service_callback);
         size_t ns_pos = topic.find('/', 1);
-        std::string service_name = topic.substr(0, ns_pos) + "/start_stop";
+        name = topic.substr(1, ns_pos-1);
+        std::string service_name = std::string("/") + name + "/start_stop";
         printf("Service name: %s\n", service_name.c_str());
         client = node_handle->serviceClient<rosmon_msgs::StartStop>(service_name, std::bind(&LaunchState::service_callback, this, std::placeholders::_1, std::placeholders::_2));
         //sub = node_handle->subscribe<rosmon_msgs::State>(topic, simple_callback);
@@ -131,9 +138,10 @@ struct LaunchState {
 const char* LaunchState::status[] = { "IDLE", "RUNNING", "CRASHED", "WAITING" };
 
 // ros stuff
-roswasm::NodeHandle* nh; 
 roswasm::ServiceClient* topics_service;
 std::unordered_map<std::string, LaunchState*> launch_states;
+roswasm::NodeHandle* nh;
+roswasm::Timer* timer;
 
 void service_callback(const rosapi::TopicsForType::Response& res, bool result)
 {
@@ -143,6 +151,86 @@ void service_callback(const rosapi::TopicsForType::Response& res, bool result)
         }
     }
 }
+
+void timer_callback(const ros::TimerEvent& event)
+{
+    rosapi::TopicsForType::Request req;
+    req.type = "rosmon_msgs/State";
+    topics_service->call<rosapi::TopicsForType>(req);
+}
+
+void init_monlaunch(roswasm::NodeHandle* n)
+{
+    nh = n;
+    topics_service = nh->serviceClient<rosapi::TopicsForType>("/rosapi/topics_for_type", service_callback);
+    rosapi::TopicsForType::Request req;
+    req.type = "rosmon_msgs/State";
+    topics_service->call<rosapi::TopicsForType>(req);
+    timer = nh->createTimer(5., timer_callback);
+}
+
+void show_monlaunch_window(bool& show_another_window)
+{
+      ImGui::SetNextWindowSize(ImVec2(550,680), ImGuiCond_FirstUseEver);
+      ImGui::Begin("Mon launch instances", &show_another_window);
+      for (std::pair<const std::string, LaunchState*>& state : launch_states) {
+          if (state.second->msg.nodes.empty()) {
+             continue;
+          } 
+          if (ImGui::CollapsingHeader(state.second->name.c_str(), ImGuiTreeNodeFlags_Framed | ImGuiTreeNodeFlags_DefaultOpen)) {
+              bool nodes_expanded = ImGui::TreeNodeEx("Nodes", ImGuiTreeNodeFlags_AllowItemOverlap | ImGuiTreeNodeFlags_FramePadding);
+              ImGui::SameLine(); //(0, 0);
+              if (ImGui::Button("Start"))
+                  state.second->start_stop_launch(rosmon_msgs::StartStop::Request::START);
+              ImGui::SameLine();
+              if (ImGui::Button("Stop"))
+                  state.second->start_stop_launch(rosmon_msgs::StartStop::Request::STOP);
+              ImGui::SameLine();
+              char buf[32];
+              float progress = state.second->get_progress();
+              sprintf(buf, "%d/%d", int(progress*state.second->msg.nodes.size()), int(state.second->msg.nodes.size()));
+              ImGui::ProgressBar(progress, ImVec2(0.f,0.f), buf);
+              if (nodes_expanded) {
+                    ImGui::Columns(5, "mycolumns"); // 4-ways, with border
+                    ImGui::Separator();
+                    ImGui::Text("Name"); ImGui::NextColumn();
+                    ImGui::Text("State"); ImGui::NextColumn();
+                    ImGui::Text("CPU"); ImGui::NextColumn();
+                    ImGui::Text("Memory"); ImGui::NextColumn();
+                    ImGui::Text("Restarts"); ImGui::NextColumn();
+                    ImGui::Separator();
+                    for (int i = 0; i < state.second->msg.nodes.size(); i++)
+                    {
+                        const std::string& name = state.second->msg.nodes[i].name;
+                        const std::string& ns = state.second->msg.nodes[i].ns;
+                        if (ImGui::Selectable(name.c_str(), state.second->selected == i, ImGuiSelectableFlags_SpanAllColumns))
+                            state.second->selected = i;
+                        if (ImGui::BeginPopupContextItem(name.c_str()))
+                        {
+                            if (ImGui::Selectable("START")) state.second->start_stop(rosmon_msgs::StartStop::Request::START, name, ns);
+                            if (ImGui::Selectable("STOP")) state.second->start_stop(rosmon_msgs::StartStop::Request::STOP, name, ns);
+                            if (ImGui::Selectable("RESTART")) state.second->start_stop(rosmon_msgs::StartStop::Request::RESTART, name, ns);
+                            ImGui::EndPopup();
+                        }
+                        bool hovered = ImGui::IsItemHovered();
+                        ImGui::NextColumn();
+                        ImGui::Text("%s", LaunchState::status[state.second->msg.nodes[i].state]); ImGui::NextColumn();
+                        ImGui::Text("%.2f %%", 100.*state.second->msg.nodes[i].user_load); ImGui::NextColumn();
+                        ImGui::Text("%.2f MB", 1e-6*state.second->msg.nodes[i].memory); ImGui::NextColumn();
+                        ImGui::Text("%u", state.second->msg.nodes[i].restart_count); ImGui::NextColumn();
+                    }
+                    ImGui::Columns(1);
+                    ImGui::Separator();
+                    ImGui::TreePop();
+              }
+          }
+      }
+      ImGui::End();
+}
+
+} // namespace roswasm_webgui
+
+roswasm::NodeHandle* nh; 
 
 GLFWwindow* g_window;
 ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
@@ -200,60 +288,7 @@ void loop()
   // 2. Show another simple window. In most cases you will use an explicit Begin/End pair to name your windows.
   if (show_another_window)
   {
-      ImGui::Begin("Mon launch instances", &show_another_window);
-      for (std::pair<const std::string, LaunchState*>& state : launch_states) {
-          if (state.second->msg.nodes.empty()) {
-             continue;
-          } 
-          if (ImGui::CollapsingHeader(state.second->topic.c_str(), ImGuiTreeNodeFlags_Framed | ImGuiTreeNodeFlags_DefaultOpen)) {
-              bool nodes_expanded = ImGui::TreeNodeEx("Nodes", ImGuiTreeNodeFlags_AllowItemOverlap | ImGuiTreeNodeFlags_FramePadding);
-              ImGui::SameLine(); //(0, 0);
-              if (ImGui::Button("Start"))
-                  state.second->start_stop_launch(rosmon_msgs::StartStop::Request::START);
-              ImGui::SameLine();
-              if (ImGui::Button("Stop"))
-                  state.second->start_stop_launch(rosmon_msgs::StartStop::Request::STOP);
-              ImGui::SameLine();
-              char buf[32];
-              float progress = state.second->get_progress();
-              sprintf(buf, "%d/%d", int(progress*state.second->msg.nodes.size()), int(state.second->msg.nodes.size()));
-              ImGui::ProgressBar(progress, ImVec2(0.f,0.f), buf);
-              if (nodes_expanded) {
-                    ImGui::Columns(5, "mycolumns"); // 4-ways, with border
-                    ImGui::Separator();
-                    ImGui::Text("Name"); ImGui::NextColumn();
-                    ImGui::Text("State"); ImGui::NextColumn();
-                    ImGui::Text("CPU"); ImGui::NextColumn();
-                    ImGui::Text("Memory"); ImGui::NextColumn();
-                    ImGui::Text("Restarts"); ImGui::NextColumn();
-                    ImGui::Separator();
-                    for (int i = 0; i < state.second->msg.nodes.size(); i++)
-                    {
-                        const std::string& name = state.second->msg.nodes[i].name;
-                        const std::string& ns = state.second->msg.nodes[i].ns;
-                        if (ImGui::Selectable(name.c_str(), state.second->selected == i, ImGuiSelectableFlags_SpanAllColumns))
-                            state.second->selected = i;
-                        if (ImGui::BeginPopupContextItem(name.c_str()))
-                        {
-                            if (ImGui::Selectable("START")) state.second->start_stop(rosmon_msgs::StartStop::Request::START, name, ns);
-                            if (ImGui::Selectable("STOP")) state.second->start_stop(rosmon_msgs::StartStop::Request::STOP, name, ns);
-                            if (ImGui::Selectable("RESTART")) state.second->start_stop(rosmon_msgs::StartStop::Request::RESTART, name, ns);
-                            ImGui::EndPopup();
-                        }
-                        bool hovered = ImGui::IsItemHovered();
-                        ImGui::NextColumn();
-                        ImGui::Text("%s", LaunchState::status[state.second->msg.nodes[i].state]); ImGui::NextColumn();
-                        ImGui::Text("%.2f %%", 100.*state.second->msg.nodes[i].user_load); ImGui::NextColumn();
-                        ImGui::Text("%.2f MB", 1e-6*state.second->msg.nodes[i].memory); ImGui::NextColumn();
-                        ImGui::Text("%u", state.second->msg.nodes[i].restart_count); ImGui::NextColumn();
-                    }
-                    ImGui::Columns(1);
-                    ImGui::Separator();
-                    ImGui::TreePop();
-              }
-          }
-      }
-      ImGui::End();
+      roswasm_webgui::show_monlaunch_window(show_another_window);
   }
 
   // 3. Show the ImGui demo window. Most of the sample code is in ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
@@ -345,10 +380,7 @@ extern "C" int main(int argc, char** argv)
   if (init() != 0) return 1;
 
   nh = new roswasm::NodeHandle();
-  topics_service = nh->serviceClient<rosapi::TopicsForType>("/rosapi/topics_for_type", service_callback);
-  rosapi::TopicsForType::Request req;
-  req.type = "rosmon_msgs/State";
-  topics_service->call<rosapi::TopicsForType>(req);
+  roswasm_webgui::init_monlaunch(nh);
 
   #ifdef __EMSCRIPTEN__
   emscripten_set_main_loop(loop, 20, 1);
