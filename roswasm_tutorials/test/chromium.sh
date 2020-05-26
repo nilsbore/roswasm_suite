e0f3c1f6e21b29c84d4ac600ed80eb738d27466f
Nils Bore <nbore@kth.se>
2020-05-26 21:51:53 +0200

    Trying to correct a flag error in CI

--- roswasm_tutorials/test/chromium.sh
+++ roswasm_tutorials/test/chromium.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sleep 1
-chromium-browser http://localhost:8080/ --headless --disable-gpu --remote-debugging-port=9222
+chromium-browser http://localhost:8080/ --headless --disable-gpu --no-sandbox --remote-debugging-port=9222
