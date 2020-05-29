e1ad097976c9c3482d19f5145b7a5582f7e534e8
Nils Bore <nilsbore@gmail.com>
2020-05-29 22:11:55 +0200

    Update run_local_ci.sh

--- run_local_ci.sh
+++ run_local_ci.sh
@@ -1,1 +1,1 @@
-rosrun industrial_ci run_ci ROS_DISTRO=melodic ROS_REPO=ros-shadow-fixed DOCKER_RUN_OPTS="-e EMSDK -e EM_CONFIG=/root/.emscripten -e EM_CACHE -e EMSDK_NODE -v ${EMSDK}:${EMSDK} -v ${EM_CONFIG}:/root/.emscripten"
+rosrun industrial_ci run_ci ROS_DISTRO=melodic ROS_REPO=main DOCKER_RUN_OPTS="-e EMSDK -e EM_CONFIG -e EM_CACHE -e EMSDK_NODE -v ${EMSDK}:${EMSDK}"
